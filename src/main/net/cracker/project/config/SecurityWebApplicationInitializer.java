19c477463d4cbd44686b73b1b3407691c633756f
YanovAlexander <yanov.alexander@gmail.com>
2017-11-22 18:33:11 +0200

    Inital commit.

---
+++ src/main/net/cracker/project/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package main.net.cracker.project.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Артём on 20.11.2017.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Test!</h2>
+</body>
+</html>

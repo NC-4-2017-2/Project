dc7737dcdc3d21689460b4b638dcd50ea4168696
YanovAlexander <yanov.alexander@gmail.com>
2017-11-29 22:51:51 +0200

    ERP-15 Add WorkingDayBuilder

--- src/main/com/netcracker/project/model/entity/WorkingDay.java
+++ src/main/com/netcracker/project/model/entity/WorkingDay.java
@@ -9,54 +9,81 @@ public class WorkingDay {
   private Date date;
   private Integer weekNumber;
   private Double workingHours;
-  private Boolean status;
+  private Status status;
   private BigInteger pmId;
+
+  private WorkingDay(WorkingDayBuilder builder) {
+    this.userId = builder.userId;
+    this.date = builder.date ;
+    this.weekNumber = builder.weekNumber;
+    this.workingHours = builder.workingHours;
+    this.status = builder.status;
+    this.pmId = builder.pmId;
+  }
 
   public BigInteger getUserId() {
     return userId;
-  }
-
-  public void setUserId(BigInteger userId) {
-    this.userId = userId;
   }
 
   public Date getDate() {
     return date;
   }
 
-  public void setDate(Date date) {
-    this.date = date;
-  }
-
   public Integer getWeekNumber() {
     return weekNumber;
-  }
-
-  public void setWeekNumber(Integer weekNumber) {
-    this.weekNumber = weekNumber;
   }
 
   public Double getWorkingHours() {
     return workingHours;
   }
 
-  public void setWorkingHours(Double workingHours) {
-    this.workingHours = workingHours;
-  }
-
-  public Boolean getStatus() {
+  public Status getStatus() {
     return status;
-  }
-
-  public void setStatus(Boolean status) {
-    this.status = status;
   }
 
   public BigInteger getPmId() {
     return pmId;
   }
 
-  public void setPmId(BigInteger pmId) {
-    this.pmId = pmId;
+  public static class WorkingDayBuilder{
+    private BigInteger userId;
+    private Date date;
+    private Integer weekNumber;
+    private Double workingHours;
+    private Status status;
+    private BigInteger pmId;
+
+    public WorkingDayBuilder(){
+    }
+
+    public WorkingDayBuilder userId(BigInteger userId) {
+      this.userId = userId;
+      return this;
+    }
+
+    public WorkingDayBuilder date(Date date) {
+      this.date = date;
+      return this;
+    }
+
+    public WorkingDayBuilder weekNumber(Integer weekNumber) {
+      this.weekNumber = weekNumber;
+      return this;
+    }
+
+    public WorkingDayBuilder workingHours(Double workingHours) {
+      this.workingHours = workingHours;
+      return this;
+    }
+
+    public WorkingDayBuilder status(Status status) {
+      this.status = status;
+      return this;
+    }
+
+    public WorkingDayBuilder pmId(BigInteger pmId) {
+      this.pmId = pmId;
+      return this;
+    }
   }
-  }
+}

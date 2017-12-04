066ae920dc07b0c0b9a6ec029c0390c8231a044e
KunitsynArtyom <KunitsynArtyom@users.noreply.github.com>
2017-12-04 23:59:07 +0200

    ERP-14 Security CSRF disabled

--- src/main/com/netcracker/project/config/WebSecurityConfig.java
+++ src/main/com/netcracker/project/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -25,5 +26,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("admin").password("admin").roles("ADMIN");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
     }
 }

7e0498a55832be070270c0403d252aa6172f16a2
YanovAlexander <yanov.alexander@gmail.com>
2018-01-08 21:13:30 +0200

    ERP-31 - Added validateCreateVacation, validatePmAndLmStatus.

--- src/com/netcracker/project/controllers/validators/VacationValidator.java
+++ src/com/netcracker/project/controllers/validators/VacationValidator.java
@@ -1,14 +1,11 @@
 package com.netcracker.project.controllers.validators;
 
+import com.netcracker.project.controllers.validators.errorMessage.ErrorMessages;
+import com.netcracker.project.model.entity.User;
+import com.netcracker.project.model.enums.ProjectStatus;
 import java.util.Map;
 
 public class VacationValidator extends AbstractValidator {
-
-  public Map<String, String> validateDates(String startDate, String endDate) {
-    validateStartEndDate(startDate, endDate);
-    return getErrorMap();
-  }
-
   public Map<String, String> validateVacationStatus(String status) {
     validateStatus(status);
     return getErrorMap();
@@ -18,4 +15,37 @@ public class VacationValidator extends AbstractValidator {
     validateId(id);
     return getErrorMap();
   }
+
+  public Map<String, String> validateCreateVacation(String startDate,
+      String endDate, User user) {
+    validateStartEndDate(startDate, endDate);
+    if (user.getProjectStatus().name()
+        .equals(ProjectStatus.TRANSIT.name())) {
+      setErrorToMap("USER_ON_TRANSIT_ERROR",
+          ErrorMessages.USER_ON_TRANSIT_ERROR);
+    }
+    return getErrorMap();
+  }
+
+  public Map<String, String> validatePmAndLmStatus(String pmStatus,
+      String lmStatus) {
+    validateStatus(pmStatus);
+    validateStatus(lmStatus);
+    return getErrorMap();
+  }
+
+  public Map<String, String> validateUpdateAuthor(String vacationId,
+      String startDate, String endDate) {
+    validateId(vacationId);
+    validateStartEndDate(startDate, endDate);
+    return getErrorMap();
+  }
+
+  public Map<String, String> validateVacationIdAndStatus(String vacationId,
+      String status) {
+    validateId(vacationId);
+    validateStatus(status);
+    return getErrorMap();
+  }
 }
+
